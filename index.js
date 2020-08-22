69cb13f550393bb7f0c45fe5d7105a09fecc1b8d
Liz P <parisielizabeth@gmail.com>
2020-08-22 23:04:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -76,5 +76,5 @@ function findEmployeeByFirstName(records, firstName){
 }
 
 function calculatePayroll(arrayOfEmployees){
-  return arrayOfEmployees.reduce((total, hours) => total + allWagesFor.call(hours));
+  return arrayOfEmployees.reduce((total, hours) => total + allWagesFor.call(hours), 0);
 }
